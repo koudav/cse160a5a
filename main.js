dedbe339bb76c1a1763f72f469598b0359019806
David Kou <davidzkou@gmail.com>
2024-04-22 22:42:25 -0700

    init2

--- main.js
+++ main.js
@@ -14,17 +14,60 @@ function main() {
 
 	const scene = new THREE.Scene();
 
+	{
+
+		const color = 0xFFFFFF;
+		const intensity = 3;
+		const light = new THREE.DirectionalLight( color, intensity );
+		light.position.set( - 1, 2, 4 );
+		scene.add( light );
+
+	}
+
 	const boxWidth = 1;
 	const boxHeight = 1;
 	const boxDepth = 1;
 	const geometry = new THREE.BoxGeometry( boxWidth, boxHeight, boxDepth );
 
-	const material = new THREE.MeshBasicMaterial( { color: 0x44aa88 } ); // greenish blue
+	function makeInstance( geometry, color, x ) {
 
-	const cube = new THREE.Mesh( geometry, material );
-	scene.add( cube );
+		const material = new THREE.MeshPhongMaterial( { color } );
 
-	renderer.render( scene, camera );
+		const cube = new THREE.Mesh( geometry, material );
+		scene.add( cube );
+
+		cube.position.x = x;
+
+		return cube;
+
+	}
+
+	const cubes = [
+		makeInstance( geometry, 0x44aa88, 0 ),
+		makeInstance( geometry, 0x8844aa, - 2 ),
+		makeInstance( geometry, 0xaa8844, 2 ),
+	];
+
+	function render( time ) {
+
+		time *= 0.001; // convert time to seconds
+
+		cubes.forEach( ( cube, ndx ) => {
+
+			const speed = 1 + ndx * .1;
+			const rot = time * speed;
+			cube.rotation.x = rot;
+			cube.rotation.y = rot;
+
+		} );
+
+		renderer.render( scene, camera );
+
+		requestAnimationFrame( render );
+
+	}
+
+	requestAnimationFrame( render );
 
 }
 
